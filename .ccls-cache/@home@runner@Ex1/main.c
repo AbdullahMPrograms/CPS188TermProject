129118f78beaf09f6b60cd146f7c7ba29f5a165f
AbdullahMProgra <105071246+AbdullahMPrograms@users.noreply.github.com>
2023-03-28 03:07:17 +0000

    Basically Everything

--- .ccls-cache/@home@runner@Ex1/main.c
+++ .ccls-cache/@home@runner@Ex1/main.c
@@ -35,9 +35,7 @@ void parseFile(FILE *file, int *year, char location[][MAX_LENGTH],
         sscanf(token, "\"%d\"", &year[i]);
       } else if (fieldcount == 1) {
         sscanf(token, "\"%[^\"]\"", location[i]);
-
         // Update start and end index of each location
-
         // if the string in location[i] is identical to "Canada (excluding
         // territories)" strcmp will return 0 (true)
         if (strcmp(location[i], "Canada (excluding territories)") == 0) {
@@ -87,15 +85,6 @@ void parseFile(FILE *file, int *year, char location[][MAX_LENGTH],
   }
 
   *numLines = i;
-  /* test code (ensure data is properly being read)
-    printf("Number of Lines: %d\n", i);
-    printf("\nData for Canada:\n");
-    printf("Year, Location, Age Group, Sex, Value\n");
-    for (int i = *canadaStart; i <= *canadaEnd; i++) {
-      printf("%d, %s, %s, %s, %.2lf\n", year[i], location[i], ageGroup[i],
-    sex[i], value[i]);
-    }
-    */
 }
 
 int main() {
@@ -111,7 +100,6 @@ int main() {
   char sex[MAX_LINES][MAX_LENGTH];
   double value[MAX_LINES];
   int numLines = 0;
-  double sum = 0;
 
   int canadaStart = -1, quebecStart = -1, ontarioStart = -1, albertaStart = -1,
       bcStart = -1;
@@ -124,20 +112,270 @@ int main() {
 
   fclose(file);
 
-  // print location start and end array index  values for testing
-  printf("Canada index values: %d to %d\n", canadaStart, canadaEnd);
-  printf("Quebec index values: %d to %d\n", quebecStart, quebecEnd);
-  printf("Ontario index values: %d to %d\n", ontarioStart, ontarioEnd);
-  printf("Alberta index values: %d to %d\n", albertaStart, albertaEnd);
-  printf("British Columbia: %d to %d\n", bcStart, bcEnd);
-
-  // test to see if averaging val does work
-  double average;
+  // < ---------------- Q1 ---------------->
+  // < ---------------- a) ---------------->
+  int count = 0;
+  double sum = 0;
+  // Quebec average is an array of size 7 to accomodate the yearly averages
+  // index 0 will be the average excluding years
+  // 1-7 will be the years 2015-2021
+  double quebecAverage[7];
+  sum = 0;
   for (int j = quebecStart; j <= quebecEnd; j++) {
-    sum = sum + value[j];
+    if (value[j] != 0) {
+      sum += value[j];
+      count++;
+    }
   }
-  average = sum / (quebecEnd - quebecStart);
-  printf("1a) Average for Quebec: %.2lf\n", average);
+  quebecAverage[0] = sum / count;
+  printf("1a) Average for Quebec: %.2lf%\n", quebecAverage[0]);
+
+  double ontarioAverage[7];
+  count = 0;
+  sum = 0;
+  for (int j = ontarioStart; j <= ontarioEnd; j++) {
+    if (value[j] != 0) {
+      sum += value[j];
+      count++;
+    }
+  }
+  ontarioAverage[0] = sum / count;
+  printf("1a) Average for Ontario: %.2lf%\n", ontarioAverage[0]);
+
+  count = 0;
+  sum = 0;
+  double bcAverage[7];
+  for (int j = bcStart; j <= bcEnd; j++) {
+    if (value[j] != 0) {
+      sum += value[j];
+      count++;
+    }
+  }
+  bcAverage[0] = sum / count;
+  printf("1a) Average for British Columbia: %.2lf%\n", bcAverage[0]);
+
+  count = 0;
+  sum = 0;
+  double albertaAverage[7];
+  for (int j = albertaStart; j <= albertaEnd; j++) {
+    if (value[j] != 0) {
+      sum += value[j];
+      count++;
+    }
+  }
+  albertaAverage[0] = sum / count;
+  printf("1a) Average for Alberta: %.2lf%\n", albertaAverage[0]);
+
+  // < ---------------- b) ---------------->
+  printf("\n");
+  count = 0;
+  sum = 0;
+  double canadaAverage[7];
+  for (int j = canadaStart; j <= canadaEnd; j++) {
+    if (value[j] != 0) {
+      sum = sum + value[j];
+      count++;
+    }
+  }
+  canadaAverage[0] = sum / count;
+  printf("1b) Average for Canada: %.2lf%\n", canadaAverage[0]);
+
+  // < ---------------- c) ---------------->
+  // 7 years in file (2015-2021)
+  // j increments by 7 to go from one 2015 to another 2015
+  // i will increment the year 2015 by an amount to go to the next year
+  // (2015 --> 2016 --> 2017 etc...)
+
+  printf("\n");
+  for (int i = 1; i <= 7; i++) {
+    count = 0;
+    sum = 0;
+    for (int j = canadaStart + i - 1; j <= canadaEnd; j += 7) {
+      if (value[j] != 0) {
+        sum = sum + value[j];
+        count++;
+        canadaAverage[i] = sum / count;
+      }
+    }
+    count = 0;
+    sum = 0;
+    for (int j = ontarioStart + i - 1; j <= ontarioEnd; j += 7) {
+      if (value[j] != 0) {
+        sum = sum + value[j];
+        count++;
+        ontarioAverage[i] = sum / count;
+      }
+    }
+    count = 0;
+    sum = 0;
+    for (int j = quebecStart + i - 1; j <= quebecEnd; j += 7) {
+      if (value[j] != 0) {
+        sum = sum + value[j];
+        count++;
+        quebecAverage[i] = sum / count;
+      }
+    }
+    count = 0;
+    sum = 0;
+    for (int j = bcStart + i - 1; j <= bcEnd; j += 7) {
+      if (value[j] != 0) {
+        sum = sum + value[j];
+        count++;
+        bcAverage[i] = sum / count;
+      }
+    }
+    count = 0;
+    sum = 0;
+    for (int j = albertaStart + i - 1; j <= albertaEnd; j += 7) {
+      if (value[j] != 0) {
+        sum = sum + value[j];
+        count++;
+        albertaAverage[i] = sum / count;
+      }
+    }
+
+    printf("1c) Average for year %d:\nCanada: %.2lf%\nOntario: %.2lf%\nQuebec: "
+           "%.2lf%\nBC: %.2lf%\nAlberta: %.2lf%\n\n",
+           (2015 + i - 1), canadaAverage[i], ontarioAverage[i],
+           quebecAverage[i], bcAverage[i], albertaAverage[i]);
+  }
+  // < ---------------- d) ---------------->
+
+  // < ---------------- Q2 ---------------->
+  // initialize min and max variables
+  double min = quebecAverage[0];
+  double max = quebecAverage[0];
+  char *lowestLocation = "Quebec";
+  char *highestLocation = "Quebec";
+
+  // compare with the other variables
+  if (bcAverage[0] < min) {
+    min = bcAverage[0];
+    lowestLocation = "British Columbia";
+  }
+  if (albertaAverage[0] < min) {
+    min = albertaAverage[0];
+    lowestLocation = "Alberta";
+  }
+  if (ontarioAverage[0] < min) {
+    min = ontarioAverage[0];
+    lowestLocation = "Ontario";
+  }
+
+  if (bcAverage[0] > max) {
+    max = bcAverage[0];
+    highestLocation = "British Columbia";
+  }
+  if (albertaAverage[0] > max) {
+    max = albertaAverage[0];
+    highestLocation = "Alberta";
+  }
+  if (ontarioAverage[0] > max) {
+    max = ontarioAverage[0];
+    highestLocation = "Ontario";
+  }
+
+  printf("2) The province with the lowest average is: %.2lf in %s\n", min,
+         lowestLocation);
+  printf("2) The province with the greatest average is: %.2lf in %s\n", max,
+         highestLocation);
+
+  // < ---------------- Q3 ---------------->
+  printf("\n");
+  printf("3) Provinces with averages above the national average:\n");
+  if (bcAverage[0] > canadaAverage[0]) {
+    printf("British Columbia\n");
+  }
+  if (ontarioAverage[0] > canadaAverage[0]) {
+    printf("Ontario\n");
+  }
+  if (albertaAverage[0] > canadaAverage[0]) {
+    printf("Alberta\n");
+  }
+  if (quebecAverage[0] > canadaAverage[0]) {
+    printf("Quebec\n");
+  }
+  printf("\n");
+  printf("3) Provinces with averages below the national average:\n");
+  if (bcAverage[0] < canadaAverage[0]) {
+    printf("British Columbia\n");
+  }
+  if (ontarioAverage[0] < canadaAverage[0]) {
+    printf("Ontario\n");
+  }
+  if (albertaAverage[0] < canadaAverage[0]) {
+    printf("Alberta\n");
+  }
+  if (quebecAverage[0] < canadaAverage[0]) {
+    printf("Quebec\n");
+  }
+  // < ---------------- Q4 ---------------->
+  printf("\n");
+  // Find the highest value and its location
+  double highestValue = quebecAverage[1];
+  int highestYear = 2015;
+
+  for (int i = 1; i <= 7; i++) {
+    if (quebecAverage[i] > highestValue) {
+      highestValue = quebecAverage[i];
+      highestYear = 2014 + i;
+      highestLocation = "Quebec";
+    }
+
+    if (bcAverage[i] > highestValue) {
+      highestValue = bcAverage[i];
+      highestYear = 2014 + i;
+      highestLocation = "British Columbia";
+    }
+
+    if (albertaAverage[i] > highestValue) {
+      highestValue = albertaAverage[i];
+      highestYear = 2014 + i;
+      highestLocation = "Alberta";
+    }
+
+    if (ontarioAverage[i] > highestValue) {
+      highestValue = ontarioAverage[i];
+      highestYear = 2014 + i;
+      highestLocation = "Ontario";
+    }
+  }
+
+  // Find the lowest value and its location
+  double lowestValue = quebecAverage[1];
+  int lowestYear = 2015;
+
+  for (int i = 1; i <= 7; i++) {
+    if (quebecAverage[i] < lowestValue) {
+      lowestValue = quebecAverage[i];
+      lowestYear = 2014 + i;
+      lowestLocation = "Quebec";
+    }
+
+    if (bcAverage[i] < lowestValue) {
+      lowestValue = bcAverage[i];
+      lowestYear = 2014 + i;
+      lowestLocation = "British Columbia";
+    }
+
+    if (albertaAverage[i] < lowestValue) {
+      lowestValue = albertaAverage[i];
+      lowestYear = 2014 + i;
+      lowestLocation = "Alberta";
+    }
+
+    if (ontarioAverage[i] < lowestValue) {
+      lowestValue = ontarioAverage[i];
+      lowestYear = 2014 + i;
+      lowestLocation = "Ontario";
+    }
+  }
+
+  // Print the results
+  printf("4) The Province and year with the highest value is: %.2f in %s, %d\n",
+         highestValue, highestLocation, highestYear);
+  printf("4) The Province and year with the lowest value is: %.2f in %s, %d\n",
+         lowestValue, lowestLocation, lowestYear);
 
   return 0;
 }
